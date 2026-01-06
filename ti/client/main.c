b6f5177545b1cb4c22b5f7b1a49959342ab10c25
Marko Maksimovic <marko.maksimovic@htecgroup.com>
2026-01-06 17:09:16 +0100

    Implement client (A72) compiling and flashing.

---
+++ ti/client/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <TI/tivx.h>
+#include <utils/ipc/include/app_ipc.h>
+#include <utils/remote_service/include/app_remote_service.h>
+#include <utils/app_init/include/app_init.h>
+#include <utils/console_io/include/app_log.h>
+#include "jpeg_compression.h"
+
+int main()
+{
+    printf("Hello world!\n");
+    return 1;
+}
