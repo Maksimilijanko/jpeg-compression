62cf5b6bdcd3966eba11089988929653c1611c9f
Marko Maksimovic <marko.maksimovic@htecgroup.com>
2026-01-15 16:36:30 +0100

    Use 1/qt to avoid division in quantization

--- ti/service/src/quantization.c
+++ ti/service/src/quantization.c
@@ -4,6 +4,7 @@
 void quantize_block(float *dct_block, int16_t* out_quantized_block) {
     uint32_t i = 0;
     for(i = 0; i < 64; i++) {
-        out_quantized_block[i] = (int16_t)roundf(dct_block[i] / std_lum_qt[i]);         // rounding to nearest integer
+        float rec = 1 / std_lum_qt[i];              // use this to force multiply
+        out_quantized_block[i] = (int16_t)roundf(dct_block[i] * rec);         // rounding to nearest integer
     }
 }
